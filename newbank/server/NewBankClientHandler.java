da00143067db6bf86f0aad20d805cbf816c26d14
Lorenz <las209@bath.ac.uk>
2021-11-12 19:31:26 +0100

    Also added the bug fixed for the Login error.

--- newbank/server/NewBankClientHandler.java
+++ newbank/server/NewBankClientHandler.java
@@ -7,18 +7,18 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class NewBankClientHandler extends Thread{
-	
+
 	private NewBank bank;
 	private BufferedReader in;
 	private PrintWriter out;
-	
-	
+
+
 	public NewBankClientHandler(Socket s) throws IOException {
 		bank = NewBank.getBank();
 		in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 		out = new PrintWriter(s.getOutputStream(), true);
 	}
-	
+
 	public void run() {
 		// keep getting requests from the client and processing them
 		try {
@@ -31,7 +31,7 @@ public class NewBankClientHandler extends Thread{
 			out.println("Checking Details...");
 			// authenticate user and get customer ID token from bank for use in subsequent requests
 			CustomerID customer = bank.checkLogInDetails(userName, password);
-			// if the user is authenticated then get requests from the user and process them 
+			// if the user is authenticated then get requests from the user and process them
 			if(customer != null) {
 				out.println("Log In Successful. What do you want to do?");
 				while(true) {
@@ -42,7 +42,9 @@ public class NewBankClientHandler extends Thread{
 				}
 			}
 			else {
-				out.println("Log In Failed");
+				out.println("Log In Failed.");
+				out.println("Please type \"EXIT\" to terminate the Application.");
+				String request = in.readLine();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
